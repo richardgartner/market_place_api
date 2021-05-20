60790d4f0d3f9d8abb7e61b4a13c3f556cc763a9
Richard Gartner <richardgartner@gmail.com>
2021-05-19 22:38:40 -0700

    add in users create end point

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,23 @@
 class Api::V1::UsersController < ApplicationController
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   def show
     render json: User.find(params[:id])
+  end
+
+  private
+
+  # only allow a trusted params "white list" through
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -13,4 +13,17 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(self.response.body)
     assert_equal @user.email, json_response['email']
   end
+
+  test 'should create user' do
+    assert_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: 'jimbob@test.com', password: 'password'}}, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test 'should not create user with taken email' do
+    post api_v1_users_url, params: { user: { email: @user.email, password: 'password'}}, as: :json
+    assert_response :unprocessable_entity
+  end
 end
